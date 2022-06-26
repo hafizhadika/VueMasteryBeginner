1edf86449b40f61ee896d9dec683ae43fb6c70cd
Hafizh Adika <hafizh.adika@inxsoftware.com>
2022-06-26 07:48:32 +0700

    Skeleton code

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import EventList from "../views/EventList.vue";
+import About from "../views/AboutView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "EventList",
+    component: EventList,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: About,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/EventList.vue
@@ -0,0 +1,66 @@
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 5928101,
+          category: "Animal Welfare",
+          title: "Cat Adoption Day",
+          description: "Find your new feline friend at this event.",
+          location: "Meow Town",
+          date: "January 28, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Kat Laydee",
+        },
+        {
+          id: 5928102,
+          category: "Animal Welfare2",
+          title: "Cat Adoption Day2",
+          description: "Find your new feline friend at this event.2",
+          location: "Meow Town2",
+          date: "January 28, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Kat Laydee2",
+        },
+        {
+          id: 5928103,
+          category: "Animal Welfare3",
+          title: "Cat Adoption Day3",
+          description: "Find your new feline friend at this event.3",
+          location: "Meow Town3",
+          date: "January 28, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Kat Laydee3",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
