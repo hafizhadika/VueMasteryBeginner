115259cb8ae9c38daf53e0bfb6f2fe4548e7ea98
Hafizh Adika <hafizh.adika@inxsoftware.com>
2022-06-26 16:06:22 +0700

    Fix issues

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,5 +1,8 @@
 <template>
-  <router-link class="event-link" :to="{ name: 'EventDetails', params: { id: event.id } }">
+  <router-link
+    class="event-link"
+    :to="{ name: 'EventDetails', params: { id: event.id } }"
+  >
     <div class="event-card">
       <span>@{{ event.time }} on {{ event.date }}</span>
       <h4>{{ event.title }}</h4>
@@ -31,7 +34,6 @@ export default {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
-
 
 .event-link {
   color: #2c3e50;
